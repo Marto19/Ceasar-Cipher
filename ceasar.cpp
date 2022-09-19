cd75112b345aed9e0f3bfd8ed760a767974779e9
Martin Trenkov <bigguissss@gmail.com>
2022-09-19 06:13:10 -0400

    Update ceasar.cpp

--- ceasar.cpp
+++ ceasar.cpp
@@ -2,11 +2,11 @@
 
 //The following function recieves text and the shift amount as parameters
 //and returns the encrypted text
-string encrypt(string text, int shift){
-    string result = "";
+std::string encrypt(std::string text, int shift){
+    std::string result = "";
 
     for (int i = 0; i < text.length(); i++){
         if(isupper(text[i])){
-            result += char(int(text[i] + s - 65)%26 +  65);
+            result += char(int(text[i] + shift - 65)%26 +  65);
         }
 }
