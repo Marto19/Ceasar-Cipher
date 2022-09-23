091bcc1a92ed27eb895ee0196fa6759f85d5ee46
Martin Trenkov <bigguissss@gmail.com>
2022-09-23 12:20:31 +0300

    Update decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 //The following function recieves the cyphered message and returns it decrypted
-std::string decrypt(std::string message, int shift){}
+std::string decrypt(std::string message, int shift){
+    for(int i =0; i < message.length(); ++i){
+
+    }
+}
 
 int main()
 {
-    std::cout << "Enter the message: ";
+    std::cout << "Enter the crypted message message: ";
     std::string message;
     std::cin >> message;
     std::cout << "Your decrypted message is: " << std::endl;
