1a2a2e1260e3dbb59e99050e9cde1fd6208b94d3
Martin Trenkov <bigguissss@gmail.com>
2022-09-23 12:09:44 +0300

    Update ceasar.cpp

--- ceasar.cpp
+++ ceasar.cpp
@@ -5,9 +5,9 @@
 std::string encrypt(std::string text, int shift){
     std::string result = "";
 
-    for (int i = 0; i < text.length(); i++){
+    for (int i = 0; i < text.length(); ++i){
         if(isupper(text[i])){
-            result += char(int(text[i] + shift - 65)%26 +  65);
+            result += char(int(text[i] + shift - 65)%26 + 65);
         }
         else{
             result += char(int(text[i] + shift - 65)%26 + 65);
@@ -16,15 +16,17 @@ std::string encrypt(std::string text, int shift){
     }
 }
 
-int main(){
-    int shift;
+int main()
+{
     std::string text;
     std::cout << "Enter text: ";
     std::cin >> text;
+    int shift;
     std::cout << std::endl;
     std::cout << "Enter shift: ";
     std::cin >> shift;
     std::cout << "Your text is: " << text << std::endl;
     std::cout << "Your shift is: " << shift << std::endl;
     std::cout << "Encrypted text is: " << encrypt(text, shift) << std::endl;
+    return 0;
 }
